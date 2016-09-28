e40b49b1771d7e1c7fcc9cd84b251fd6b88e3cbb
collnell <coll.s.nell#gmail.com>
2016-09-28 15:04:58 -0700

    advanced version of beans app

---
+++ server.R
@@ -0,0 +1,223 @@
+library(shiny)
+library(ggplot2)
+library(rhandsontable)
+library(dplyr)
+library(devtools)
+library(readr)
+
+ttable<-read.csv("https://raw.githubusercontent.com/collnell/beans/master/ttable.csv")#ttable
+source_url("https://raw.githubusercontent.com/collnell/R_vis/master/theme_mooney.R")#ggplot theme
+
+se <- function(x) sqrt(var(x)/length(x))
+ci95 <-function(x) se(x)*ttable[ttable$n == length(x),2]
+
+shinyServer(function(input,output){
+  
+  ###reactive variable names & variables
+  output$tr1<-renderText({input$treat1})
+  output$tr2<-renderText({input$treat2})
+  output$var1<-renderText({input$var1})
+  output$var2<-renderText({input$var2})
+  ##make blank dfs
+  values <- reactiveValues(df_data1 = data.frame(variable1 = as.numeric(rep(NA,50)),
+                                                variable2 = as.numeric(rep(NA,50))),
+                           df_data2 =data.frame(variable1 = as.numeric(rep(NA,50)),
+                                                variable2 = as.numeric(rep(NA,50))))
+  values$df_data = data.frame(variable1= as.numeric(rep(0,100)),
+                                  variable2=as.numeric(rep(0,100)),
+                                  Treatment = as.factor(rep("treat1",100)),
+                                  Proportion1 = as.numeric(rep(0,100)))
+  values$summ_data = data.frame(Treatment = c("treat1","treat2"),
+                                mean_var1 =c(0,0),
+                                mean_var2=c(0,0),
+                                mean_prop = c(0,0),
+                                n_df = c(0,0),
+                                se_prop = c(0,0),
+                                sd_prop = c(0,0),
+                                ci_prop = c(0,0))
+  
+  ##generate talbes
+  output$table1 = renderRHandsontable({
+    pvar1<-paste0("Proportion(",input$var1,")")
+    rhandsontable(values$df_data1,width=300,height=485,colHeaders =c(input$var1,input$var2,pvar1))
+  })
+  output$table2 = renderRHandsontable({
+    pvar1<-paste0("Proportion(",input$var1,")")
+    rhandsontable(values$df_data2,width=300,height=485,colHeaders =c(input$var1,input$var2,pvar1))
+  })
+
+  observeEvent(input$getdata, { 
+    df_data1<-hot_to_r(input$table1)##update df
+    df_data2<-hot_to_r(input$table2)
+    df_data1$Treatment<-rep(input$treat1,length(values$df_data1$variable1))#make treatment var
+    df_data2$Treatment<-rep(input$treat2,length(values$df_data2$variable1))
+    df_data_all<-rbind(df_data1,df_data2)#bind
+    df_data_all$Proportion1<-df_data_all$variable1/(df_data_all$variable1+df_data_all$variable2)#calc proportions
+    df_data_all$Proportion2<-df_data_all$variable2/(df_data_all$variable1+df_data_all$variable2)
+    
+    summdata<-df_data_all%>%#summarize data
+      na.omit()%>%###need to fix data summary box to show all variables?
+      group_by(Treatment)%>%
+      summarize(mean_prop1 = mean(Proportion1), #col2
+                mean_prop2 = mean(Proportion2),#col3
+                mean_var1 = mean(variable1),#col4
+                mean_var2 = mean(variable2),#col5
+                n_v1 = length(variable1), 
+                n_v2 =length(variable2),
+                se_prop=se(Proportion1), 
+                sd_prop = sd(Proportion1), 
+                ci_prop = ci95(Proportion1))
+    
+    values$df_data<-df_data_all
+    values$summ_data<-summdata
+    values$inputcols<-summdata
+  })
+ 
+  output$selectUI<-renderUI({
+    validate(need(input$getdata,"Treatment and variable names will update when data is run")
+    )
+    v1<-paste0(input$var1)
+    v2<-paste0(input$var2)
+    p1<-paste0("Proportion (",input$var1,")")
+    p2<-paste0("Proportion (",input$var2,")")
+    colnames(values$inputcols)<-c("Treatment",p1,p2,v1,v2)#rename variables based on textinput
+    selectInput("figval","Y-axis value:",colnames(values$inputcols[,2:5]))#dynamic ui that changes with inputs
+    ##the problem here is that it is impossible to connect with the plot then...
+  })
+
+ #plot means
+  output$plot1 <- renderPlot({
+    v1<-paste0(input$var1)
+    v2<-paste0(input$var2)
+    p1<-paste0("Proportion (",input$var1,")")
+    p2<-paste0("Proportion (",input$var2,")")
+    
+    validate(need(input$getdata,"Plot will appear here"))
+    if (input$figval == p1){##what i could do is have a selection for which proportion? and then calcualte proportion, and viola!
+      yval<-values$summ_data$mean_prop1
+    } else if (input$figval == p2) {
+      yval<-values$summ_data$mean_prop2
+    } else if (input$figval == v1){
+      yval<-values$summ_data$mean_var1
+    } else {
+      yval<-values$summ_data$mean_var2
+    }
+    
+    error<-switch(input$errortype,##reactive error bars
+                   se=values$summ_data$se_prop,
+                   sd=values$summ_data$sd_prop,
+                   ci=values$summ_data$ci_prop)
+    
+    yvar.lab<-paste("Proportion",input$var1)#reactive y axis label based on variable inputs
+    ##eventually want the plotted variable to be a drop-down so that it could be proportion var1 or var2
+    
+    bar.plot<-ggplot(values$summ_data,aes_string(x='Treatment',y=yval,fill='Treatment'))+#plot
+      geom_bar(stat="identity")+
+      theme_mooney()+
+      geom_errorbar(aes(ymax=yval+error,ymin=yval-error),width=.2)+
+      labs(x="Treatment",y=yvar.lab)+
+      scale_fill_manual(values=c("#006666","#FF9900"))+
+      theme(legend.position="none")
+    bar.plot
+  })
+  
+  output$histo<- renderPlot({
+    validate(need(input$getdata,"Histogram will appear here"))
+    
+    v1<-paste0(input$var1)
+    v2<-paste0(input$var2)
+    p1<-paste0("Proportion (",input$var1,")")
+    p2<-paste0("Proportion (",input$var2,")")
+    
+    if (input$figval == p1){
+      yval<-values$summ_data$mean_prop1
+      meanval<-values$df_data$Proportion1##these names are backwards
+      xval<-p1
+    } else if (input$figval == p2) {
+      yval<-values$summ_data$mean_prop2
+      meanval<-values$df_data$Proportion2
+      xval<-p2
+    } else if (input$figval == v1){
+      yval<-values$summ_data$mean_var1
+      meanval<-values$df_data$variable1
+      xval<-v1
+    } else {
+      yval<-values$summ_data$mean_var2
+      meanval<-values$df_data$variable2
+      xval<-v2
+    }
+    
+    
+    hist.plot<-ggplot(data=values$df_data,aes_string(x=meanval,fill='Treatment',color='Treatment'))+
+      geom_density(alpha=.35)+
+      theme_mooney(legend.location="bottom")+
+      labs(x=yval,y="Frequency")+
+      scale_fill_manual(values=c("#006666","#FF9900"))+
+      scale_color_manual(values=c("#006666","#FF9900"))
+    
+    if(input$showmean == FALSE){
+      hist.plot
+    } else{
+      hist.plot+geom_vline(data=values$summ_data,aes_string(xintercept=yval,color='Treatment'),
+                           linetype="dotted",size=2)+
+        geom_text(data=values$summ_data,aes_string
+                  (x=yval, label=round(yval,2), y=0), color="black", angle=90, hjust=0,vjust=0)
+    }
+  })
+  
+  
+  output$summary_table<- renderRHandsontable({
+    validate(
+      need(input$getdata, "Enter values and press 'Run Data' for summary statistics")
+    )
+    stat.df<-values$df_data%>%
+      na.omit()%>%
+      group_by(Treatment)%>%
+      summarize(N = length(Proportion1),
+                SE = se(Proportion1),
+                S = sd(Proportion1),
+                mean = mean(Proportion1),
+                t = ttable[ttable$n == length(Proportion1),2],
+                CI = ci95(Proportion1),
+                CIlow = mean(Proportion1)-ci95(Proportion1),
+                CIhi = mean(Proportion1)-ci95(Proportion1))
+    rhandsontable(stat.df[,-1], rowHeaders=c(input$treat1,input$treat2),
+                  colHeaders=c("N","SE","S","Mean","T","95% CI","Low CI","High CI"),readOnly=TRUE,
+                  rowHeaderWidth=130)%>%
+      hot_cols(colWidths = 60)
+  })
+
+  
+  output$anovatable<-renderPrint({
+    validate(
+      need(input$getdata, "Enter values and press 'Run Data' for ANOVA test results")
+    )
+    aov.model<-aov(Proportion1~Treatment,data=values$df_data)
+    print(aov.model)
+    br()
+    br()
+    print(summary(aov.model))
+  })
+  
+
+  ##download csv of data
+  output$downloadData <-downloadHandler(
+    filename = function() {
+      paste("data-",Sys.Date(),".csv", sep="")
+    },
+    content = function(file){
+      write.csv(testdf1,file)
+    }
+  )
+  output$downloadplotr <-downloadHandler(##this should be redone
+    filename=function(){
+      paste("ggplot_bar.r")
+    },
+    content =function(file){
+      write_rds(bar.plot,file,compress="none")
+    }
+  )
+
+  
+})
+
---
+++ ui.R
@@ -0,0 +1,68 @@
+
+library(shinydashboard)
+library(rhandsontable)
+##need to figure out how to have conditional messages if there is not enough data yet to fill blank boxes
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Example",tabName="example",icon=icon("stats",lib="glyphicon")),
+    menuItem("test",tabName="test"),br(),
+    textInput("treat1","Treatment 1:",value="Treatment 1"),
+    textInput("treat2","Treatment 2:", value="Treatment 2"),
+    textInput("var1","Variable 1:", value= "A"),
+    textInput("var2","Variable 2:",value= "B")
+  
+  ) 
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName="example",
+      fluidRow(
+        box(title = "Comparing and plotting means",width=3,solidHeader=TRUE,status="primary",collapsible = TRUE,
+            p("Use the text boxes on the left to name the treatments and variables. Enter data in the respective tables."),br(),
+            p("Pressing 'Run Data' initiates a one-way ANOVA test on the data entered in the tables. 
+              This test evaluates whether the variable of interest differs between the two treatment groups"),br(),
+            p("Summary statistics will also be calculated and plotted. Use the controls to select which variables are plotted."),br(),
+            p("Note: data must be entered for both treatments in order for the outputs to work."),br(),
+            actionButton("getdata","Run Data"),br(),br(),
+            p("To replicate these analyses in R, use download the data and code that produces these outputs."),
+            downloadButton("downloadData","Download Data"),
+            downloadButton("downloadplotr","Get code")
+        ),
+        box(title = "Data Entry", width=5,solidHeader = TRUE,status="primary",
+            column(width=5,textOutput("tr1"), rHandsontableOutput("table1")),
+            column(width=5,textOutput("tr2"),rHandsontableOutput("table2"))
+        ),
+        tabBox(id = "plots",width = 4,
+            tabPanel("Plotting means",plotOutput("plot1"),
+              radioButtons("errortype", "Error bars:", 
+                           choices=c("95% Confidence interval"="ci","Standard error (SE)"="se","Standard deviation (S)"="sd")),
+              htmlOutput("selectUI")
+              ),
+            tabPanel("Histogram",plotOutput("histo"),
+              checkboxInput("showmean","Show means", value=FALSE),
+              htmlOutput("selectUI"))
+        )
+      ),
+      fluidRow(
+        box(title="Data Summary", width = 6,status="primary",
+            textOutput("vartext"),
+            rHandsontableOutput("summary_table"), br(),br()
+        ),
+        box(title="One-way ANOVA", width=6, status="primary",
+            verbatimTextOutput("anovatable"))
+      )
+    ),
+    tabItem(tabName="test",
+            h2("this is a test")
+    )
+  )
+)
+
+# Put them together into a dashboardPage
+dashboardPage(skin = "red",
+  dashboardHeader(title = "beans"),
+  sidebar,
+  body
+)
